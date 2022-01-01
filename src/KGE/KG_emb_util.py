bf5a26f4d1cb309fd5c3196b0e6d7f2c11e71b65
Debanjan Datta <ddatta@vt.edu>
2022-01-01 17:46:56 -0500

    update:: KG distmult in stellargraph

---
+++ src/KGE/KG_emb_util.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+import numpy as np
+import os
+import sys
+sys.path.append('./..')
+import pandas as pd
+import yaml
+from pathlib import Path
+import pickle
+from collections import OrderedDict
+import faiss  
+from typing import *
+from joblib import Parallel, delayed
+import multiprocessing as MP
+# -------------------------
+file_path = os.path.realpath(__file__)
+print(file_path)
+CONFIG_FILE = os.path.join( os.path.basename(file_path) ,'config.yaml')
+
+class node_fetcher:
+    def __init__(self, DIR, embedding_dim = None):
+        self.DIR = DIR
+        self.embedding_dim = embedding_dim
+        self.faiss_index = None
+        self.load_embeddings()
+        
+        return 
+    
+    def load_embeddings(self):
+        global CONFIG_FILE
+        with open('config.yaml', 'r') as fh:
+            config = yaml.safe_load(fh)
+        if self.embedding_dim  is None:
+            self.embedding_dim  = config['embedding_dimension']
+        emb_save_dir = os.path.join(config['kg_emb_save_dir'], self.DIR)
+        with open(os.path.join(emb_save_dir, 'KG_DM_nodeEmb_{}.pkl'.format(self.embedding_dim)), 'rb') as fh:
+            self.node_emb_dict = pickle.load(fh)
+        
+        with open(os.path.join(emb_save_dir, 'KG_DM_edgeEmb_{}.pkl'.format(self.embedding_dim)), 'rb') as fh:
+            self.edge_emb_dict = pickle.load(fh)
+            
+        
+        # -------------------------------------------
+        # Create mapping dict 
+        # ------------------------------------------
+        idx = 0
+        self.serialId_2_entityID = OrderedDict({})
+        node_emb_array = []
+        for domain, emb_dict in self.node_emb_dict.items():
+            for _id,_emb in emb_dict.items(): 
+                entity_key = domain + '_' + str(_id)
+                self.serialId_2_entityID[idx] = entity_key
+                idx += 1
+                node_emb_array.append(_emb)
+        self.node_emb_array = np.array(node_emb_array).astype(np.float32)
+        
+        self.faiss_index = faiss.IndexFlatIP(self.embedding_dim) 
+        self.faiss_index.add(self.node_emb_array)  
+     
+        return
+    
+                               
+    
+    '''
+    Distmult calculates similarity as inner product <head, rel, tail>
+    also note DistMult is symmetric
+    head: [ domain, entity_id ]
+    rel : [start node domain, end node domain]
+    '''
+    def find_NN(
+        self, 
+        head: List, 
+        rel: List, 
+        num_NN:int = 10
+    ):
+        domain, entity_id = head[0], head[1]
+        
+        head_emb = self.node_emb_dict[domain][entity_id]
+        edge_type = '_'.join(sorted(rel))
+        edge_emb = self.edge_emb_dict[edge_type]
+        
+        
+        y = np.zeros([self.embedding_dim,self.embedding_dim])
+        np.fill_diagonal( y, edge_emb)
+        
+        x = np.inner(head_emb.reshape([1,-1]),y).astype(np.float32)
+        
+        rel.remove(domain)
+        tail_domain = rel[0]
+        
+        # Use an expanding search approach --- nearest neighbors may not be of desired domain type
+        k = num_NN * 100
+        res = []
+        
+        def aux(_idx,_dist):
+            d_entity = self.serialId_2_entityID[_idx] # d_entity has form <domain>_<entity_id>
+            # Validate 
+            cand_dom = d_entity.split('_')[0]
+            if cand_dom != tail_domain: 
+                return None
+            return (d_entity.split('_')[1],_dist)
+            
+        while True:
+            # Find the nearest neighbors
+            
+            dist , fetched_idx = self.faiss_index.search(x, k) 
+            fetched_idx = fetched_idx[0].tolist()
+            dist = dist[0].tolist()
+
+            res = Parallel(
+                n_jobs = MP.cpu_count(),prefer="threads"
+            )(delayed(aux) ( _idx, _dist,) for _idx, _dist in zip(fetched_idx, dist ))
+            res = [ _ for _ in res if _ is not None]
+            
+            if len(res)>= num_NN:
+                res = res[:num_NN]
+                break
+            else:
+                k = int(k * 2.5) 
+            
+        res = sorted(res, key=lambda x: x[1], reverse=True )
+        
+        NN = [_[0] for _ in res]  
+        return NN
+                    
+        
+
+
+
+
+
+'''
+Example usage
+'''
+# obj = node_fetcher('us_import1')
+# obj.find_NN( 
+#         head =[ 'Carrier',10],
+#         rel = ['Carrier', 'HSCode']
+# )
+
+
+
---
+++ src/KGE/create_embeddings.py
@@ -0,0 +1,224 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import torch
+import os
+import sys
+import yaml
+import numpy as np
+sys.path.append('./..')
+import pandas as pd
+import pickle
+import stellargraph
+from pathlib import Path
+from stellargraph import StellarGraph
+from stellargraph.mapper import *
+from stellargraph.layer import DistMult,regularizers
+from stellargraph import datasets, utils
+from tensorflow.keras import callbacks, optimizers, losses, metrics, regularizers, Model
+from collections import OrderedDict
+# from IPython.display import HTML
+import argparse
+
+DATA_DIR = None
+DIR = None
+id_col = 'PanjivaRecordID'
+CONFIG_FILE = 'config.yaml'
+KG_emb_dim = 64
+kg_train_numNegSamples = 10
+domain_dims = None
+KG_emb_save_dir = None
+KG_train_batchsize = 512
+KG_train_patience = 10
+KG_train_epochs = 100
+
+# ----------------------------------------------
+
+def setup_config(_DIR):
+    global DATA_DIR, DIR, KG_emb_dim, kg_train_num_neg_samples, domain_dims
+    global KG_emb_save_dir
+    global KG_train_epochs
+    global KG_train_patience
+    global KG_train_batchsize
+    DIR = _DIR
+    with open(CONFIG_FILE,'r') as fh:
+        config = yaml.safe_load(fh)
+    DATA_DIR = config['DATA_DIR']
+    KG_emb_dim = config['embedding_dimension']
+    kg_train_num_neg_samples  = config ['kg_train_num_neg_samples']
+    domain_dims = pd.read_csv( os.path.join(DATA_DIR, _DIR, 'data_dimensions.csv'), index_col= None)
+    KG_emb_save_dir = os.path.join(config['kg_emb_save_dir'], _DIR)
+    KG_train_epochs = config['kg_train_epochs']
+    KG_train_patience = config['kg_train_epochs']
+    KG_train_batchsize = config['kg_train_batch_size']
+    Path(os.path.join(KG_emb_save_dir, _DIR)).mkdir(exist_ok=True, parents=True)
+    
+    return 
+
+
+
+def get_graphTrainingData():
+    global DATA_DIR, DIR, id_col
+    with open('metapaths.txt','r') as fh:
+        mp = fh.readlines()
+    mp = [ _.strip('\n') for _ in mp]
+    mp = [ _.split(',') for _ in mp]
+    # Create data
+    df_data_file = os.path.join(DATA_DIR, DIR, 'train_data.csv')
+    df = pd.read_csv(df_data_file, index_col = None)
+
+    try:
+        del df[id_col]
+    except:
+        pass
+
+    df_named = df.copy(deep=True)
+    columns = list(df_named.columns)
+
+    for col in columns:
+        df_named[col] = df_named[col].apply(lambda x : col+'_'+ str(x))
+
+    relationships = set()
+    for _mp in mp:
+        for i in range(len(_mp)-1):
+            relationships = relationships.union( [(_mp[i],_mp[i+1])])
+
+
+    edge_data_wLabels = pd.DataFrame(columns=['source','label', 'target'])
+    for relation in relationships:
+        _columns = list(relation)
+        df_tmp = df_named[_columns]
+        df_tmp = df_tmp.rename(columns = {_columns[0]:'source', _columns[1]:'target'})
+        df_tmp.loc[:,'label'] = '_'.join( sorted([_columns[0], _columns[1]]))
+        df_w = df_tmp.groupby(['source', 'target']).size().reset_index(name='weight')
+        df_tmp = df_tmp.merge(df_w, on =['source','target'], how='inner')
+        edge_data_wLabels = edge_data_wLabels.append(df_tmp,ignore_index=True)
+        edge_data_wLabels = edge_data_wLabels.drop_duplicates(subset =['source', 'target']).reset_index(drop=True)
+    node_dict = {}
+    for column in list(df_named.columns):
+        nodes = list(sorted(set(df_named[column])))
+        node_dict[column] = pd.DataFrame( None, index= nodes)
+    return node_dict, edge_data_wLabels
+
+'''
+Main function to train the model
+'''
+
+def train_model():
+    global KG_train_epochs
+    global KG_train_patience
+    global KG_emb_dim
+    global KG_emb_save_dir
+    global KG_train_batchsize
+    
+    node_dict, edge_data_wLabels = get_graphTrainingData()
+    
+    sg = StellarGraph(
+        node_dict, 
+        edge_data_wLabels,
+        edge_type_column="label"
+    )
+
+    _gen = KGTripleGenerator(
+        sg,
+        batch_size=KG_train_batchsize  # ~10 batches per epoch
+    )
+
+    distmult = DistMult(
+        _gen,
+        embedding_dimension=KG_emb_dim,
+        embeddings_regularizer=regularizers.l2(1e-7),
+    )
+
+    _inp, _out = distmult.in_out_tensors()
+    _model = Model(
+        inputs=_inp, 
+        outputs=_out
+    )
+    _model.compile(
+        optimizer=optimizers.Adam(lr=0.001),
+        loss=losses.BinaryCrossentropy(from_logits=True),
+        metrics=[metrics.BinaryAccuracy(threshold=0.0)],
+    )
+
+
+    _train_gen = _gen.flow(
+        edge_data_wLabels, 
+        negative_samples=kg_train_num_neg_samples, 
+        shuffle=True
+    )
+
+    earlyStop_callback = callbacks.EarlyStopping(
+        monitor="binary_accuracy", 
+        patience=KG_train_patience
+    )
+
+    _model.fit(
+        _train_gen,
+        epochs=KG_train_epochs,
+        callbacks = [earlyStop_callback],
+        verbose=1,
+    )
+    '''
+    Obtain embeddings of node and relations
+    '''
+
+    node_embeddings = distmult.embeddings()[0]
+    relation_embeddings = distmult.embeddings()[1]
+    domains_list = list(domain_dims['column'].values)
+    node_emb_dict = OrderedDict( { _ : OrderedDict({}) for _ in domains_list }  )
+    relation_emb_dict = {}
+
+    for entity,emb in zip(list(sg.nodes()),node_embeddings):
+        dom, e = entity.split('_')
+        e = int(e)
+        node_emb_dict[dom][e] = emb
+
+
+    for idx,e_type in enumerate(sg.edge_types,0):
+        relation_emb_dict[e_type] = relation_embeddings[idx]
+
+    '''
+    Save KGE embeddings
+    '''
+    nodeEmb_saveFile_name = 'KG_DM_nodeEmb_{}.pkl'.format(KG_emb_dim)
+    edgeEmb_saveFile_name = 'KG_DM_edgeEmb_{}.pkl'.format(KG_emb_dim)
+
+    with open(os.path.join(KG_emb_save_dir, nodeEmb_saveFile_name), 'wb') as fh:
+        pickle.dump(node_emb_dict, fh, pickle.HIGHEST_PROTOCOL)
+    
+    with open(os.path.join(KG_emb_save_dir, edgeEmb_saveFile_name), 'wb') as fh:
+        pickle.dump(relation_emb_dict, fh, pickle.HIGHEST_PROTOCOL)
+    print('[INFO]  Saved embeddings at :',  os.path.join(KG_emb_save_dir, edgeEmb_saveFile_name), os.path.join(KG_emb_save_dir, edgeEmb_saveFile_name))
+    return
+
+# -----------------------------------------------------------------------------------------
+parser = argparse.ArgumentParser(description='Generate anomalies')
+parser.add_argument('--dir', type = str, help='Which dataset ? us__import{1,2...}' )
+args = parser.parse_args()
+DIR = args.dir
+
+setup_config(DIR)
+train_model()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
