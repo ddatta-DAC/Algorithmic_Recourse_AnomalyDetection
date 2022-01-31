7896016ef1072d451ca486e34d3fee276f37c03a
Debanjan Datta <ddatta@vt.edu>
2022-01-31 08:29:30 -0500

    save

--- src/Metrics/conditional_correctness.py
+++ src/Metrics/conditional_correctness.py
@@ -28,7 +28,9 @@ except:
     base_path = './'
 
 config_file = os.path.join(base_path, 'cc_config.yaml')
-print(config_file)
+
+
+
 
 class condCorrectnessEvaluator:
     def __init__(
@@ -48,12 +50,12 @@ class condCorrectnessEvaluator:
         self.ad_model_emb = self.config['ad_model_emb'] 
         self.AD_obj = AD_processor(self.dataset)
         
-        score_df_file = os.path.join(base_path, 'CC_reference_df.csv')
+        score_df_file = os.path.join(base_path, 'CC_reference_df_{}.csv'.format(dataset))
         if os.path.exists(score_df_file) and refresh is False:
             self.score_df = pd.read_csv(score_df_file, index_col=None)
         else:
             self.score_df = self.get_scores_df(samples_frac)
-            self.score_df.to_csv(score_df_file)
+            self.score_df.to_csv(score_df_file,index= False)
         return
     
     def get_scores_df(self, samples_frac):
@@ -69,7 +71,6 @@ class condCorrectnessEvaluator:
                 anomaly_df = anomaly_df.append(df)
 
         normal_df = pd.read_csv(os.path.join(base_path, normal_df_path, self.dataset,  'train_data.csv'), index_col=None)
-        
         anomaly_df = anomaly_df.sample(frac = samples_frac)
         normal_df = normal_df.sample(frac = samples_frac)
 
@@ -86,46 +87,24 @@ class condCorrectnessEvaluator:
         self.score_df = pd.DataFrame(data = data, columns = [self.data_id_col, 'score'])
         self.score_df = self.score_df.sort_values(by = ['score'])
         self.score_df[ self.data_id_col] = self.score_df[ self.data_id_col ].astype(int)
-
+        self.score_df[ self.data_id_col] = np.arange(1,len(self.score_df)+1).astype(int)
         self.score_df = self.score_df.reset_index(drop=True)
         return self.score_df 
-            
+    
+    def get_rank(self, record):
+        ref_values = self.score_df['score'].values
+        new_val = record['score']
+        rank = np.searchsorted(ref_values , new_val)
+        return rank 
+        
     def calculate(
         self, 
-        record_ref : pd.DataFrame,  # anomaly
+        rank_1, # : pd.DataFrame,  # anomaly
         record_target: pd.DataFrame  # counterfactual
     ):
-        # print(record_ref)
-        # print(record_target)
         
-        def get_rank(record):
-            df = self.score_df.copy(deep=True)
-            df = df.reset_index(drop=True)
-            
-            
-            record = pd.DataFrame(record).reset_index(drop=True)
-            new_id = int('9999999' + str(record[self.data_id_col].values[0])[:4] )
-            record.loc[0,self.data_id_col] = new_id
-            _id = new_id
-
-            new_score =  self.AD_obj.score_samples_batch(
-                pd.DataFrame(record), 
-                model_emb_dim = self.ad_model_emb
-            )
-            
-            record['score'] = new_score[0]
-            record = pd.DataFrame(record)[[self.data_id_col, 'score']]
-            
-            df = df.append(record, ignore_index=True)
-            df = df.sort_values(by = ['score'])
-            df = df.reset_index(drop=True)
-            rank = df.loc[df[self.data_id_col]==_id].index[0]
-            del df
-            return rank 
-        
-        
-        rank_1 = get_rank(record_ref) 
-        rank_2 = get_rank(pd.DataFrame([record_target]))
+        # rank_1 = get_rank(record_ref) 
+        rank_2 = self.get_rank(pd.DataFrame([record_target]))
         
         res = float( rank_1 < rank_2 )
         return res
@@ -135,13 +114,29 @@ class condCorrectnessEvaluator:
             record_ref,
             record_targets
         ):
-        res = []
+        
+        scores =  self.AD_obj.score_samples_batch(
+                pd.DataFrame(record_targets), 
+                model_emb_dim = self.ad_model_emb
+        )
+        record_targets['score'] = scores
+        ref_score =  self.AD_obj.score_samples_batch(
+                    pd.DataFrame(record_ref), 
+                    model_emb_dim = self.ad_model_emb
+                )
+        record_ref['score'] = ref_score[0]
+        
+        rank_1 = self.get_rank(record_ref) 
         
         # for i, record_target in record_targets.iterrows():
         #     r = self.calculate(record_ref, record_target)
         #     res.append(r)
-        res = Parallel(n_jobs=MP.cpu_count(), prefer="threads")(delayed(self.calculate)(record_ref, record_target) for i, record_target in record_targets.iterrows())
-        # print (res)
+        res = []
+        # for i, record_target in record_targets.iterrows():
+        #     r = self.calculate(rank_1, record_target)
+        #     res.append(r)
+        res = Parallel(n_jobs=MP.cpu_count(), prefer="threads")(delayed(self.calculate)(rank_1, record_target) for i, record_target in record_targets.iterrows())
+        
         return np.mean(res)                                    
 
 # 
